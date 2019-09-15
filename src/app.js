0ea4d0228150cc18adc57b8dae83e8bfb0a2ea65
Leon Ji <mgleon08@gmail.com>
2019-09-16 00:25:56 +0800

    Setting up a 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -8,15 +8,17 @@ const ExpenseDashboardPage = () => <div>ExpenseDashboardPage</div>;
 const AddExpensePage = () => <div>AddExpensePage</div>;
 const EditExpensePage = () => <div>EditExpensePage</div>;
 const HelpPage = () => <div>HelpPage</div>;
+const NotFoundPage = () => <div>NotFoundPage</div>;
 
 const routers = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}></Route>
       <Route path="/create" component={AddExpensePage}></Route>
       <Route path="/edit" component={EditExpensePage}></Route>
       <Route path="/help" component={HelpPage}></Route>
-    </div>
+      <Route component={NotFoundPage}></Route>
+    </Switch>
   </BrowserRouter>
 );
 ReactDOM.render(routers, document.getElementById('app'));
